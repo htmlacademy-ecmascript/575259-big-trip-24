903afbc563d2ab7a149594fe2cfcb100a9c6add6
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2024-08-28 22:35:13 +0700

    add hashing

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "build"),
     clean: true,
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   plugins: [
+    new HtmlPlugin({
+      template: "public/index.html",
+    }),
     new CopyPlugin({
-      patterns: [{ from: 'public' }],
+      patterns: [
+        {
+          from: "public",
+          globOptions: {
+            ignore: ["**/index.html"],
+          },
+        },
+      ],
     }),
   ],
   module: {
@@ -20,12 +31,12 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
+            presets: ["@babel/preset-env"],
           },
         },
       },
-    ]
-  }
+    ],
+  },
 };
